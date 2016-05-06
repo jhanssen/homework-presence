a4838ffda6f79097ba8647284ebcd71f5f906551
Anders Bakken <agbakken@gmail.com>
2016-05-06 00:35:09 -0700

    Just invoke the utility I suppose.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 "use strict";
 
-
-let timeout;
 module.exports = {
     get name() { return "presence"; },
     get ready() { return true; },
@@ -18,50 +16,43 @@ module.exports = {
         if (!cfg.frequency)
             cfg.frequency = 300000; // 5 minutes
 
-        let dev = {addValue: function() {}}; //new homework.Device(homework.Device.Type.Presence);
-        let val = {update: function() {}}; //new homework.Device.Value("Presence");
+        let dev = new homework.Device(homework.Device.Type.Presence);
+        let val = new homework.Device.Value("Presence");
         val._valueType = "boolean";
-        val.update(true);
         dev.addValue(val);
         homework.addDevice(dev);
 
         let failures = 0;
-        function ping()
+        function probe()
         {
-            //Only promise wrapper supports configable ping options
-            // const pingSession = require("net-ping").createSession(cfg); // allow overriding all of the
-            let found = 0, notFound = 0;
+            let found = false, count = 0;
             cfg.devices.forEach(function(host) {
-
-                console.log("pinging host " + host);
-                ping.promise.probe(host, cfg).then(function(result) {
-                    console.log("GOT RESPONSE " + host);
-                    if (result) {
-                        ++found;
-                    } else {
-                        ++notFound;
+                // console.log("pinging host " + host);
+                ping.sys.probe(host, function(isAlive) {
+                    // console.log("GOT RESPONSE " + host + " " + isAlive);
+                    if (isAlive) {
+                        found = true;
                     }
-                    if (found + notFound == cfg.devices.length) {
+                    if (++count == cfg.devices.length) {
                         if (!found) {
                             ++failures;
                             if (failures > (cfg.tolerance || 0)) {
-                                console.log("ALL DEVICES ARE GONE");
+                                // console.log("ALL DEVICES ARE GONE");
                                 val.update(false);
                             }
-                            timeout = setTimeout(ping, cfg.errorFrequency || (cfg.frequency / 10));
                         } else {
-                            console.log("GOT DEVICES");
+                            // console.log("GOT DEVICES");
                             val.update(true);
                             failures = 0;
-                            timeout = setTimeout(ping, cfg.frequency);
                         }
+                        timeout = setTimeout(probe, cfg.frequency);
                     }
                 });
             });
         }
-        // homework.utils.onify(this);
-        // this._initOns();
-        timeout = setTimeout(ping, 5000);
+        homework.utils.onify(this);
+        this._initOns();
+        timeout = setTimeout(probe, 5000);
         return true;
     },
     shutdown: function(cb) {
@@ -73,4 +64,4 @@ module.exports = {
     }
 };
 
-module.exports.init({ devices: ["192.168.1.2"], frequency: 5000 }, {}, {addDevice: function() {}});
+// module.exports.init({ devices: ["192.168.1.2"], frequency: 5000 }, {}, {addDevice: function() {}});
